1102773e11de469e8439a298129f08dbc1cb5dba
bfin4 <bfin4444@gmail.com>
2020-10-02 08:01:49 -0400

    Added space

--- userName.py
+++ userName.py
@@ -4,7 +4,7 @@ Created on Oct 1, 2020
 @author: bfin4
 '''
 import psutil
-pid = int(input('Enter PID'))
+pid = int(input('Enter PID: '))
 p=psutil.Process(pid)
 print('User is:', psutil.Process.username(p))
 print('Command line name is:', psutil.Process.cmdline(p))
