3ac7a3a2447d3b3f9cc45895bb51552a8183f7b1
bfin4 <bfin4444@gmail.com>
2020-10-09 10:44:35 -0400

    Second test commit. 09OCT2020

--- utils0/keepalive.py
+++ utils0/keepalive.py
@@ -1,24 +1,8 @@
 '''
 Created on Oct 9, 2020
 
-@author: bfin4
+@author: bfin4444
 '''
-import time, win32com.client
 
-shell = win32com.client.Dispatch("WScript.Shell")
-shell.AppActivate("Command Prompt")
-shell.run("cmd")
-# shell2.run("cmd")
-
-shell.AppActivate("cmd")
-time.sleep(2)
-shell.SendKeys("{ENTER}")
-shell.SendKeys("dir{ENTER}")
-time.sleep(2)
-# shell.SendKeys('^c')
-# time.sleep(2)
-shell.SendKeys('exit')
-time.sleep(1)
-shell.SendKeys("{ENTER}")
 if __name__ == '__main__':
     pass
