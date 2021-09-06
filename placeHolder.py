7d2fc1c56d6f688a76da243880d8cb19fbeb845d
bfin4444 <bfin4444@gmail.com>
2021-09-06 14:43:53 -0400

    06sep2021

--- placeHolder.py
+++ placeHolder.py
@@ -1,15 +1,3 @@
 <<<<<<< HEAD
-# Initial add 05sep2021
-# 0 06sep2021
-print("placeholder")
-print("second print")
-print("third print")
-#comment
-=======
-#Comment to test git.
-# Test again
-# Third comment
-print('Hello')
-# Another comment
-# and another
+# 06sep2021
 >>>>>>> refs/remotes/origin/master
