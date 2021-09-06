c7ffc3261e0de21c57537ebb8ff16e83348149ac
bfin4444 <bfin4444@gmail.com>
2021-09-06 14:47:33 -0400

    garbage

--- placeHolder.py
+++ placeHolder.py
@@ -1,15 +1,5 @@
 <<<<<<< HEAD
 # Initial add 05sep2021
 # 0 06sep2021
-print("placeholder")
-print("second print")
-print("third print")
-#comment
-=======
-#Comment to test git.
-# Test again
-# Third comment
-print('Hello')
-# Another comment
-# and another
+
 >>>>>>> refs/remotes/origin/master
