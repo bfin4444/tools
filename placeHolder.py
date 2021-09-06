d5797673cc24622f756f3cd2c36d576d4dd07cf1
bfin4444 <bfin4444@gmail.com>
2021-09-05 21:53:41 -0400

    Test

--- placeHolder.py
+++ placeHolder.py
@@ -1,8 +1,1 @@
 # Initial add 05sep2021
-# change in feature. merge to master
-<<<<<<< HEAD
-# 2nd change in feature.
-=======
-# 2nd change in feature.
-3rd change in feature
->>>>>>> 75729fe3ab76921125a02ce94079dc09fa90ea54
