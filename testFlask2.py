400d13b3ab71873a5b3ba60f7a50005717ff8d6a
bfin4444 <bfin4444@gmail.com>
2021-09-06 20:02:16 -0400

    Add test of flask

---
+++ testFlask2.py
@@ -0,0 +1,46 @@
+# using flask_restful
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
+import time, random
+# creating the flask app
+app = Flask(__name__)
+# creating an API object
+api = Api(app)
+@app.route('/', methods=['GET'])
+
+# making a class for a particular resource
+# the get, post methods correspond to get and post requests
+# they are automatically mapped by flask_restful.
+# other methods include put, delete, etc.
+class Hello(Resource):
+
+        def get(self):
+                data = {'time': time.strftime("%Y-%m-%dT%H:%M:%S", time.localtime(time.time())), 'data': { 'baseData': { 'dimNames': [ 'sourceName', 'MessageType' ], 'metric': 'oper_item_interface_speed', 'namespace': 'Mfvs_Pleaf_PM_IF_Stats', 'series': [ { 'count': 3, 'dimValues': [ 'cbama22cwp', 'json' ], 'speed': random.randint(1,1000000), 'avg': 500000} ] } }}
+                return jsonify(data)
+
+    # Corresponds to POST request
+        def post(self):
+
+                data = request.get_json()     # status code
+                print(data)
+                return jsonify(data)
+
+
+# another resource to calculate the square of a number
+class Square(Resource):
+
+        def get(self, num):
+
+                return jsonify({'square': num**2})
+
+
+# adding the defined resources along with their corresponding urls
+api.add_resource(Hello, '/')
+api.add_resource(Square, '/square/<int:num>')
+
+
+# driver function
+if __name__ == '__main__':
+
+        app.run(debug = True)
+
